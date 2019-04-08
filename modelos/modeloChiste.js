61cf12e8761aa5c39f741b9753cc57c148f73ca2
Jos <jaas22@gmail.com>
2019-04-08 13:57:59 +0200

    version 2.0

--- modelos/modeloChiste.js
+++ modelos/modeloChiste.js
@@ -1,7 +1,35 @@
 const mysql = require('mysql');
 const conectar = require('../conectar');
 
+const login = (callback) => {
+	conectar.query('SELECT * FROM usuarios', (error, result) => {
+		if (error) throw error;
+		else {
+			return callback(error, result);
+		};
+	});
+};
 
+const todasCategorias = (callback) => {
+	conectar.query('SELECT * FROM categorias', (error, result) => {
+		if (error) throw error;
+		else {
+			return callback(error, result);
+		};
+	});
+};
+
+const introducirChiste = (chiste, callback) => {
+	conectar.query('INSERT INTO chistes (chiste, id_categoria) VALUES ("' + chiste.chiste + '", "' + chiste.id_categoria + '")', (error, result) => {
+		console.log('modelo: ' + chiste.chiste + ', ' + chiste.id_categoria);
+		if (error) throw error;
+		else {
+			return callback(error, result);
+		};
+	});
+};
 
 module.exports = {
+	todasCategorias,
+	introducirChiste
 };
--- routes/controlador.js
+++ routes/controlador.js
@@ -3,6 +3,32 @@ var router = express.Router();
 
 const modelo = require('../modelos/modeloChiste');
 
+router.get('/', (req, res) => {
+	modelo.login((error, resultado) => {
+		res.render('login', {usuarios: resultado});
+	});
+});
 
+router.post('/login', (req, res) => {
+	console.log(req.body);
+	modelo.usuariosLogin(req.body, (error, resultado) => {
+		res.render('index');
+	});
+});
+
+
+
+router.get('/introducir', (req, res) => {
+	modelo.todasCategorias((error, result) => {
+		res.render('introducirChiste', {categorias: result});
+	});
+});
+
+router.post('/introducir/chiste', (req, res) => {
+	console.log(req.body);
+	modelo.introducirChiste(req.body, (error, result) => {
+		res.render('index');
+	});
+});
 
 module.exports = router;
