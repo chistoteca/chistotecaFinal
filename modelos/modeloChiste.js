1656dd90fedf08847add4b5f43341b1490c2c120
ines <atilapresidente@gmail.com>
2019-04-09 08:35:28 +0200

    versión Inés

--- modelos/modeloChiste.js
+++ modelos/modeloChiste.js
@@ -2,6 +2,27 @@ const mysql = require('mysql');
 const conectar = require('../conectar');
 
 
+const listaCategoria = (categoria,callback)=>{
+	conectar.query('SELECT * FROM categorias', categoria, (error, resultado)=>{
+		if (error) throw error;
+		else{
+			return callback(error, resultado);
+		}
+	});
+};
+
+const consultaChiste = (id_categoria, callback) =>{
+	console.log('hola')
+	conectar.query('SELECT chiste FROM chistes WHERE id_categoria = ?', id_categoria, (error, result) =>{
+		console.log(result);
+		return callback(error, result);
+	});
+};
+
+
 
 module.exports = {
+	listaCategoria,
+	consultaChiste
+	
 };
--- routes/controlador.js
+++ routes/controlador.js
@@ -4,5 +4,19 @@ var router = express.Router();
 const modelo = require('../modelos/modeloChiste');
 
 
+router.get('/listarChiste', (req, res)=>{
+			modelo.listaCategoria(req.query.id_categoria, (error, resultado)=>{
+				res.render('listarChiste', {registros:resultado});
+			})
+		});
+
+
+router.get('/listarChiste/chistes', (req, res) => {
+	console.log(req.query)
+	modelo.consultaChiste(req.query.id_categoria, (error, result) => {
+			res.render('listarChiste', {chistes: result});
+	});
+});
+
 
 module.exports = router;
